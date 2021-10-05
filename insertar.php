d186b9d4cc239b4986659ba77f92b32d97b896bb
Leire <lvilmar1@epsg.upv.es>
2021-10-05 20:09:08 +0200

    php

---
+++ insertar.php
@@ -0,0 +1,16 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    require_once 'conexion.php';
+    $data = file_get_contents('php://input');
+    echo $data;
+    $jsonData=json_decode($data);
+    $Medicion=$jsonData->{'Medicion'};;
+    
+    $query="INSERT INTO datos (Medicion) VALUES('".$Medicion."')";
+$resultado=$mysql->query($query);
+if($resultado==true){
+    echo "Los datos se introducen de forma correcta";
+}else{
+    echo "Error al insertar datos";
+}
+}
