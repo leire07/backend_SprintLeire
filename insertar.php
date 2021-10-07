570cf3bc0241281eeeb0f1a4b7dfc3811ed85819
Leire <lvilmar1@epsg.upv.es>
2021-10-07 17:35:48 +0200

    Cambiar insertar para añadir latitud y longitud

--- insertar.php
+++ insertar.php
@@ -5,12 +5,17 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     echo $data;
     $jsonData=json_decode($data);
     $Medicion=$jsonData->{'Medicion'};;
+    $Latitud=$jsonData->{'Latitud'};;
+    $Longitud=$jsonData->{'Longitud'};;
     
-    $query="INSERT INTO tabla (Medicion) VALUES('".$Medicion."')";
-$resultado=$conn->query($query);
-if($resultado==true){
+    //$query="INSERT INTO tabla (Medicion) VALUES('".$Medicion."')";
+    $query="INSERT INTO tabla(Medicion, Latitud, Longitud) VALUES ('".$Medicion."','".$Latitud."','".$Longitud."')";
+    
+    $resultado=$conn->query($query);
+    if($resultado==true){
     echo "Los datos se introducen de forma correcta";
 }else{
     echo "Error al insertar datos";
 }
+    
 }
